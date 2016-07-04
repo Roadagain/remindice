47555897739d41f6c71fd1659e4ade1967a309df
Roadagain <ringoh72@gmail.com>
2016-07-04 17:47:28 +0900

    Change filename to expand path

--- lib/remindice.rb
+++ lib/remindice.rb
@@ -2,7 +2,7 @@ require "remindice/version"
 require "thor"
 
 module Remindice
-  TASK_FILENAME = "~/.reamindice_tasks"
+  TASK_FILENAME = File.expand_path("~/.reamindice_tasks")
   @@tasks = if File.exists?(TASK_FILENAME); File.readlines(TASK_FILENAME) else [] end
   class << self
     def tasks

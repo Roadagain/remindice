82254f02f7e4137c83abb4927f3a4a3177199c58
Roadagain <ringoh72@gmail.com>
2016-07-04 16:41:08 +0900

    Load tasks from the constant file name

--- lib/remindice.rb
+++ lib/remindice.rb
@@ -2,6 +2,8 @@ require "remindice/version"
 require "thor"
 
 module Remindice
+  TASK_FILENAME = "~/.reamindice_tasks"
+  @@tasks = if File.exists?(TASK_FILENAME); File.readlines(TASK_FILENAME) else [] end
   class Commands < Thor
   end
 end

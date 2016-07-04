7b5fa3c36047418104ec3ce205f1d13f6b0af128
Roadagain <ringoh72@gmail.com>
2016-07-04 16:42:20 +0900

    Add getter gets tasks

--- lib/remindice.rb
+++ lib/remindice.rb
@@ -4,6 +4,11 @@ require "thor"
 module Remindice
   TASK_FILENAME = "~/.reamindice_tasks"
   @@tasks = if File.exists?(TASK_FILENAME); File.readlines(TASK_FILENAME) else [] end
+  class << self
+    def tasks
+      @@tasks.clone
+    end
+  end
   class Commands < Thor
   end
 end
